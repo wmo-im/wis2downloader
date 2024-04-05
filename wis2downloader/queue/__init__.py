4dfee922805bcd315787dbdf8de4bd5ddd37f6c6
david-i-berry <dberry@wmo.int>
2024-04-05 11:43:29 +0200

    Refactor and packaging for pip install. - additional of pyproject.toml - move of code to module dir (wis2downloader) - debugging

--- wis2downloader/queue/__init__.py
+++ wis2downloader/queue/__init__.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
+import logging
 from queue import Queue
+import time
+
+from wis2downloader import shutdown
+from wis2downloader.log import LOGGER, setup_logger
 
 
 class BaseQueue(ABC):
@@ -29,10 +34,24 @@ class BaseQueue(ABC):
         """Check if the queue is empty"""
         pass
 
+    @abstractmethod
+    def is_active(self) -> bool:
+        """
+        Function that returns whether the queue is active
+        """
+        pass
+
+
+class QMonitor:
+    def __init__(self, _queue: BaseQueue, period:int = 60):
+        while not shutdown.is_set():
+            LOGGER.info(f"Queue size: {_queue.size()}")
+            time.sleep(period)
 
 class SimpleQueue(BaseQueue):
     def __init__(self):
         self._queue = Queue()
+        self.active = True
 
     def enqueue(self, item):
         self._queue.put(item)
@@ -49,6 +68,11 @@ class SimpleQueue(BaseQueue):
     def is_empty(self) -> bool:
         return self._queue.empty()
 
+    # Todo - always active, change to use flag that can be toggled
+    def is_active(self):
+        return self.active
+
+
 
 class RedisQueue(BaseQueue):
-    
+    pass
--- wis2downloader/subscriber/__init__.py
+++ wis2downloader/subscriber/__init__.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
-from paho.mqtt.client import Client
-import ssl
+from fnmatch import fnmatch
 import json
-import logging
-from wis2downloader.queuer import BaseQueue
+from pathlib import Path
+import ssl
 
-LOGGER = logging.getLogger(__name__)
+import paho.mqtt.client as mqtt
+
+from wis2downloader import shutdown
+from wis2downloader.log import LOGGER, setup_logger
+from wis2downloader.queue import BaseQueue
 
 
 class BaseSubscriber(ABC):
@@ -45,13 +48,18 @@ class BaseSubscriber(ABC):
         """Method to return list of active subscriptions"""
         pass
 
+    @abstractmethod
+    def start(self):
+        """Method to start subscriber, e.g. loop_forever in paho-mqtt"""
+        pass
+
 
 class MQTTSubscriber(BaseSubscriber):
-    def __init__(self, broker: str = "globalbroker.meteo.fr", port: int = 443, uid: str = "everyone", pwd: str = "everyone", protocol: str = "websockets", queue: BaseQueue):
+    def __init__(self, broker: str = "globalbroker.meteo.fr", port: int = 443, uid: str = "everyone", pwd: str = "everyone", protocol: str = "websockets", _queue: BaseQueue = None):
 
         LOGGER.debug("Initializing MQTT subscriber")
 
-        self.client = Client(
+        self.client = mqtt.Client(
             mqtt.CallbackAPIVersion.VERSION2, transport=protocol)
         self.client.tls_set(ca_certs=None, certfile=None, keyfile=None,
                             cert_reqs=ssl.CERT_REQUIRED,
@@ -63,31 +71,54 @@ class MQTTSubscriber(BaseSubscriber):
         self.client.on_message = self._on_message
         self.client.on_subscribe = self._on_subscribe
 
-        self.queue = queue
+        self.queue = _queue
         self.active_subscriptions = {}
 
         # Connect to the broker
         LOGGER.info("Connecting...")
+        LOGGER.info(f"Host: {broker}, port: {port}")
         self.client.connect(host=broker, port=port)
 
-    def _on_connect(self, client, userdata, flags, reason_code):
+    def _on_connect(self, client, userdata, flags, reason_code, properties ):
         if reason_code == 0:
             LOGGER.info("Connected successfully")
         elif reason_code > 0:
             LOGGER.error(f"Connection failed with error code {reason_code}")
 
-    def _on_disconnect(self, client, userdata, reason_code):
+    def _on_disconnect(self, client, userdata, disconnect_flags, reason_code, properties):
         if reason_code == 0:
             LOGGER.info("Disconnected successfully")
         elif reason_code > 0:
             LOGGER.error(f"Disconnection failed with error code {reason_code}")
 
     def _on_message(self, client, userdata, msg):
+        # first check shutdown is not set
+        if shutdown.is_set():
+            self.client.disconnect()
         LOGGER.info(f"Message received under topic {msg.topic}")
-        
+        target = self.active_subscriptions.get(msg.topic,{}).get('target')
+        # if a wild card is used in the subs target may not match
+        if target is None:
+            for key, value in self.active_subscriptions.items():
+                if fnmatch(msg.topic, value['pattern']):
+                    target = value['target']
+                    break
+
+        if target == "$TOPIC":
+            target = msg.topic
+
+        if "/" in target:
+            subdirs = target.split("/")
+            target = str(Path(*subdirs))
+
+        if "\\" in target:
+            subdirs = target.split("\\")
+            target = str(Path(*subdirs))
+
         job = {
             'topic': msg.topic,
-            'payload': json.loads(msg.payload)
+            'payload': json.loads(msg.payload),
+            'target': target
         }
         self.queue.enqueue(job)
 
@@ -101,13 +132,27 @@ class MQTTSubscriber(BaseSubscriber):
 
     def add_subscription(self, topic: str, save_path: str = "."):
         self.client.subscribe(topic)
-        self.active_subscriptions[topic] = save_path
+        self.active_subscriptions[topic] = {
+            'target' : save_path,
+            'pattern': topic.replace("+", "*").replace("#", "*")
+        }
         LOGGER.info(f"Subscribing to {topic}")
+        return self.active_subscriptions
 
     def delete_subscription(self, topic: str):
-        self.client.unsubscribe(topic)
-        del self.active_subscriptions[topic]
-        LOGGER.info(f"Unsubscribing from {topic}")
+        if topic in self.active_subscriptions:
+            self.client.unsubscribe(topic)
+            del self.active_subscriptions[topic]
+            LOGGER.info(f"Unsubscribing from {topic}")
+        else:
+            LOGGER.info(f"Topic {topic} not found in active subscriptions")
+        return self.active_subscriptions
 
     def list_subscriptions(self) -> dict:
         return self.active_subscriptions
+
+    def start(self):
+        self.client.loop_forever()
+
+    def stop(self):
+        self.client.disconnect()
