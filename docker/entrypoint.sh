feb637fa3fc81d474c5e944e3c559bd89c08f6e0
david-i-berry <dberry@wmo.int>
2024-08-19 08:13:09 +0200

    Fixing linter errors.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,21 +4,21 @@
 echo "Download directory in container: $DOWNLOAD_DIR"
 
 # ensure DOWNLOAD_DIR exists
-if [ ! -d $DOWNLOAD_DIR ]; then
+if [ ! -d "$DOWNLOAD_DIR" ]; then
     echo "Creating download directory: $DOWNLOAD_DIR"
-    mkdir -p $DOWNLOAD_DIR
+    mkdir -p "$DOWNLOAD_DIR"
 fi
 envsubst < config.template > config.json
 
 # if session-info.json does not exists in $DOWNLOAD_DIR, create it
-if [ ! -f $DOWNLOAD_DIR/.session-info.json ]; then
+if [ ! -f "$DOWNLOAD_DIR/.session-info.json" ]; then
     echo "Creating .session-info.json"
-    echo "{" > $DOWNLOAD_DIR/.session-info.json
-    echo '  "topics": {},' >> $DOWNLOAD_DIR/.session-info.json
+    echo "{" > "$DOWNLOAD_DIR/.session-info.json"
+    echo '  "topics": {},' >> "$DOWNLOAD_DIR/.session-info.json"
     # generate a random string for client_id
     echo "Generating random client_id"
-    echo '  "client_id": "wis2box-wis2downloader-'$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)'"' >> $DOWNLOAD_DIR/.session-info.json
-    echo "}" >> $DOWNLOAD_DIR/.session-info.json
+    echo '  "client_id": "wis2box-wis2downloader-'$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)'"' >> "$DOWNLOAD_DIR/.session-info.json"
+    echo "}" >> "$DOWNLOAD_DIR/.session-info.json"
 fi
 
 # print the config
