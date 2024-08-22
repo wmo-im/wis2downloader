d7028a1b6937bf85081fc48ad9ba201f21509700
david-i-berry <dberry@wmo.int>
2024-08-22 19:51:38 +0200

    back to dev

--- wis2downloader/__init__.py
+++ wis2downloader/__init__.py
@@ -20,6 +20,6 @@
 ###############################################################################
 from threading import Event
 
-__version__ = '0.3.1'
+__version__ = '0.3.dev2'
 
 stop_event = Event()
